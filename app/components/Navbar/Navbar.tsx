eb8d95c44f3a19e4be0b2e08a0600ee9fb08e0ea
Bark Developer <bark-developer@proton.me>
2024-06-20 17:24:39 +0000

    d

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,30 +1,20 @@
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import React from "react";
+import Wallet from "../swap/Wallet/Wallet";
 import Link from "next/link";
-import styles from "./NavBar.module.css";
 
-export function NavBar() {
+const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between p-3 bg-white shadow-md dark:bg-dark-bg">
-      <Link href="/" passHref>
-        <a className="flex items-center text-lg font-bold text-dark-grey dark:text-light-grey">
-          <img src="/barkswap.svg" alt="BarkSwap Logo" className="h-12 mr-2" />
-        </a>
-      </Link>
-      <ul className="flex space-x-4">
-        <li>
-          <Link href="/" passHref>
-            <a className="text-sm font-semibold text-dark-grey dark:text-light-grey">Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/swap" passHref>
-            <a className="text-sm font-semibold text-dark-grey dark:text-light-grey">Swap</a>
-          </Link>
-        </li>
-      </ul>
-      <WalletMultiButton className="font-bold bg-dark-grey text-white dark:bg-light-grey dark:text-dark-bg" />
+    <nav className="p-4 bg-white dark:bg-dark-bg shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" passHref>
+          <a className="flex items-center text-2xl font-bold text-custom-orange">
+            <img src="/barkswap.svg" alt="BarkSwap Logo" className="h-12 mr-2" />
+          </a>
+        </Link>
+        <Wallet />
+      </div>
     </nav>
   );
-}
+};
 
-export default NavBar;
+export default Navbar;
