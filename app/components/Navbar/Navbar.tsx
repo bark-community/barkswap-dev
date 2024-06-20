9abe91fa4fe222e1c4d55c4061d4a81c003048e8
Bark Developer <bark-developer@proton.me>
2024-06-20 16:32:09 +0000

    Navbar not correct and need to fix

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Wallet from "../swap/Wallet/Wallet";
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
@@ -8,22 +9,18 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" passHref>
           <a className="flex items-center text-2xl font-bold text-custom-orange">
-            <img src="/barkswap.svg" alt="BarkSwap Logo" className="h-8 mr-2" />
+            <Image src="/barkswap.svg" alt="BarkSwap Logo" width={48} height={48} className="mr-2" />
           </a>
         </Link>
         <ul className="flex items-center space-x-4 text-lg">
           <li>
             <Link href="/swap" passHref>
-              <a className="font-semibold text-gray-800 dark:text-white hover:text-custom-orange transition">
-                Swap
-              </a>
+              <a className="nav-link">Swap</a>
             </Link>
           </li>
           <li>
             <Link href="/limit-order" passHref>
-              <a className="font-semibold text-gray-800 dark:text-white hover:text-custom-orange transition">
-                Limit Order
-              </a>
+              <a className="nav-link">Limit Order</a>
             </Link>
           </li>
         </ul>
